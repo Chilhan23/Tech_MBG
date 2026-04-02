89ce7ea5455b0d15b89133673c43b92e92f52f90
Chilhan23 <chilhansaha@Gmail.com>
2026-04-02 10:40:00 +0700

    add filters for jurusan, class

--- app/Filament/Resources/Students/Schemas/StudentForm.php
+++ app/Filament/Resources/Students/Schemas/StudentForm.php
@@ -13,6 +13,7 @@ class StudentForm
         return $schema
             ->components([
                 TextInput::make('nisn')
+                    ->numeric()
                     ->unique(ignoreRecord: true)
                     ->required(),
                 TextInput::make('name')
